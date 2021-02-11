808780fd71fe182d7dad166db24d0ccffd500877
Dinar <saygafarov.dinar@gmail.com>
2021-02-12 01:54:05 +0500

    Добавил страницу "Топики"

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -6,4 +6,8 @@ app_name = 'learning_logs.urls'
 urlpatterns = [
     # Домашняя страница
     url(r'^$', views.index, name='index'),
+    # Вывод всех тем
+    url(r'^topics/$', views.topics, name='topics'),
+    # Страница с подробной информацией по отдельной теме
+    # непонятная ошибка url('r^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
