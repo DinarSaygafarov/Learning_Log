084ca754fe1881f3210c7da625e61b0b86aea179
Dinar <saygafarov.dinar@gmail.com>
2021-02-15 02:05:39 +0500

    Добавил возможность добавления записи через страницу

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
     # Страница для добавления новой темы
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    # Страница для долбавления новой записи
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
