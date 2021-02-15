dfba8a71253848953a1fcd906fdbc8d6cd47f49e
Dinar <saygafarov.dinar@gmail.com>
2021-02-16 01:22:44 +0500

    Добавил возможность редактирования созданной записи

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
     # Страница для долбавления новой записи
     url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    # Страница для редактирования записи
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
