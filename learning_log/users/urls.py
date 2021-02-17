2dc50060ef4be2ca8ad3462eb6640e9e41b64ee6
Dinar <saygafarov.dinar@gmail.com>
2021-02-17 22:58:23 +0500

    Добавил log out

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -1,12 +1,14 @@
 '''Определяет схемы URL для пользователей'''
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from .import views
 
 app_name = 'users'
 urlpatterns = [
-    # Страницы входа
-    # url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+    # Страница входа
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    # Страница выхода
+    path('logout/', LogoutView.as_view(template_name='learning_logs/index.html'), name='logout'),
 ]
